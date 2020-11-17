7bd195a08e68cf3addfc3f3f4b74d2045f5bb3bb
nicomp42 <nicholdw@ucmail.uc.edu>
2020-11-17 09:15:15 -0500

    First Commit

---
+++ ExceptionExample/src/exceptionExample/ExceptionExample.java
@@ -0,0 +1,14 @@
+/* Exception Example
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+package exceptionExample;
+
+public class ExceptionExample {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
