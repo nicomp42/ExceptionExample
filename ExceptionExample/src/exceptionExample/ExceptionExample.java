e6c5cef8629f777011fe2c690471b511774382f1
nicomp42 <nicholdw@ucmail.uc.edu>
2020-11-17 09:17:42 -0500

    Added zero-divide

--- ExceptionExample/src/exceptionExample/ExceptionExample.java
+++ ExceptionExample/src/exceptionExample/ExceptionExample.java
@@ -7,7 +7,10 @@ package exceptionExample;
 public class ExceptionExample {
 
 	public static void main(String[] args) {
-		
+		int gamma = 42;
+		int delta = 0;
+		delta = gamma / delta;	// run-time error (exception)
+	
 
 	}
 
